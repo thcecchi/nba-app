606beff448518176827e2551dd68480ed0190163
Tyler Cecchi <thcecchi@gmail.com>
2016-09-18 19:30:34 -0400

    updated key code

--- containers/Chart.js
+++ containers/Chart.js
@@ -86,7 +86,7 @@ class Chart extends Component {
               hashHistory.pushState(null, '/#/');
               e.target.value = ''
             }
-            else if(e.keyCode == 65 || e.keyCode == 66 || e.keyCode == 67 || e.keyCode == 68 || e.keyCode == 69 || e.keyCode == 70 || e.keyCode == 71 || e.keyCode == 72 || e.keyCode == 73 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 77 || e.keyCode == 78 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 81 || e.keyCode == 82 || e.keyCode == 83 || e.keyCode == 84 || e.keyCode == 85 || e.keyCode == 86 || e.keyCode == 87 || e.keyCode == 88 || e.keyCode == 89 || e.keyCode == 90) {
+            else if(e.keyCode == 65 || e.keyCode == 66 || e.keyCode == 67 || e.keyCode == 68 || e.keyCode == 69 || e.keyCode == 70 || e.keyCode == 71 || e.keyCode == 72 || e.keyCode == 73 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 77 || e.keyCode == 78 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 81 || e.keyCode == 82 || e.keyCode == 83 || e.keyCode == 84 || e.keyCode == 85 || e.keyCode == 86 || e.keyCode == 87 || e.keyCode == 88 || e.keyCode == 89 || e.keyCode == 90 || e.keyCode == 229) {
               dispatch(searchName(e.target.value, state.playerList.items))
             }
           }}/>
